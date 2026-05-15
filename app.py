ec5716c7350b330eb142fb30ae137c98d2567836
Akshita-322 <akshitapriyadarshini4@gmail.com>
2026-05-15 14:41:04 +0530

    Added model accuracy section

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
 
 # Load dataset
 data = pd.read_csv("student_data.csv")
@@ -14,14 +15,28 @@ st.write(data)
 X = data[['study_hours', 'attendance', 'sleep_hours', 'assignments']]
 y = data['marks']
 
+# Split dataset
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
 # Train model
 model = LinearRegression()
-model.fit(X, y)
+model.fit(X_train, y_train)
 
+# Predictions
+y_pred = model.predict(X_test)
+
+# Accuracy
+accuracy = r2_score(y_test, y_pred)
 # Streamlit UI
 st.title("🎓 Student Performance Prediction System")
 
 st.write("Enter student details to predict marks.")
+st.subheader("📌 Model Accuracy")
+
+st.success(f"Model Accuracy Score: {round(accuracy * 100, 2)}%")
+
 
 study_hours = st.number_input("Study Hours", min_value=0.0, max_value=24.0)
 attendance = st.number_input("Attendance Percentage", min_value=0.0, max_value=100.0)
